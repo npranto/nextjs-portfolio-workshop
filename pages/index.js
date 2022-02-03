59cdd04fd8d4e84f994eda781f6f0f9ec5060735
Nazmuz Pranto <npranto@gmail.com>
2022-02-02 20:13:44 -0500

    creates a base level portfolio app

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,38 @@
+import {
+  Heading,
+  Flex,
+  VStack,
+  HStack,
+  Icon,
+  Text,
+  Link,
+  Avatar,
+} from "@chakra-ui/react";
+
+import { FaTwitter, FaGithub } from "react-icons/fa";
+
 export default function Home() {
-	return <h1>Home Page</h1>
+  return (
+    <VStack align="center" gap="32px" justify="center">
+      <Avatar src="/profile.png" size="2xl" />
+      <VStack>
+        <Heading as="h1">Nazmuz Pranto</Heading>
+        <Heading size="md" as="h3">
+          Front End Engineer
+        </Heading>
+      </VStack>
+      <Text textAlign="center">
+        👋 Hello! I&apos;m Shakib, a Front End Engineer, love building
+        application on the web w/ React and its eco-system.
+      </Text>
+      <HStack spacing="20px" justifySelf="center">
+        <Link href="https://twitter.com/nazmuzpranto" target="_blank">
+          <Icon color="blue.200" as={FaTwitter} />
+        </Link>
+        <Link href="https://github.com/npranto" target="_blank">
+          <Icon color="black" as={FaGithub} />
+        </Link>
+      </HStack>
+    </VStack>
+  );
 }
